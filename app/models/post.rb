a6d6f89d0924f641542c257ba853a3c5b49cc14e
Baskakovs <baskakovs@me.com>
2023-01-27 23:00:39 +0000

    commit tree 8dc91b72e4a6ea92b21022edf254bcd18f4d0f74

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Non-Fiction Fiction)}
+    validate :click_baity
+
+    def click_baity
+            arr = [
+                /Won't Believe/i,
+                /Secret/i,
+                /Top [0-9]/i,
+                /Guess/
+                ]
+        if arr.none?{|phrase| phrase.match?(title)}
+            errors.add(:title, "d")
+        end
+    end
 end
